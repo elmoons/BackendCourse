c79c4a93e66be4d04222a9cc5255b3a380b0ac70
stasastapov <elmoons@yandex.ru>
2024-09-22 00:35:16 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import uvicorn
+from fastapi import FastAPI, Query, Body
+
+app = FastAPI()
+
+hotels = [
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Dubai", "name": "dubai"},
+]
+
+
+@app.get("/hotels")
+def get_hotels(
+        title: str | None = Query(default=None, description="Название отеля"),
+        id: int | None = Query(default=None, description="Айдишник")
+):
+    hotels_ = []
+    for hotel in hotels:
+        if id and id != hotel["id"]:
+            continue
+        if title and title != hotel["title"]:
+            continue
+        hotels_.append(hotel)
+    return hotels_
+
+
+@app.post("/hotels")
+def create_hotel(
+        title: str = Body(embed=True),
+):
+    global hotels
+    hotels.append({
+        "id": hotels[-1]["id"] + 1,
+        "title": title
+    })
+    return {"status": "OK"}
+
+
+@app.delete("/hotel/{hotel_id}")
+def delete_hotel(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status": "OK"}
+
+
+@app.put("/hotels/{hotel_id}")
+def put_hotel(hotel_id: int, title: str = Body(), name: str = Body()):
+    global hotels
+    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id]
+    hotel[0]["title"] = title
+    hotel[0]["name"] = name
+    return {"status": "OK"}
+
+
+# @app.patch("/hotels/{hotel_id}")
+# def patch_hotel(hotel_id: int):
+#     pass
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5999, reload=True)
