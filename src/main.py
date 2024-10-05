71248cc98a551c33581a6db0750260f0d0fa7656
stasastapov <elmoons@yandex.ru>
2024-10-05 11:35:19 +0300

    commit of first migrations

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import uvicorn
+from fastapi import FastAPI
+
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+from src.api.hotels import router as router_hotels
+from src.config import settings
+
+from src.database import *
+
+print(f"{settings.DB_URL}")
+
+app = FastAPI()
+
+app.include_router(router_hotels)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=7000, reload=True)
---
+++ src/models/hotels.py
@@ -0,0 +1,12 @@
+from src.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer
+
+
+class HotelsOrm(Base):
+    __tablename__ = "hotels"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(length=100))
+    location: Mapped[str]
+
