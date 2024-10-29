fe9c6dadaccdaaa75ff2f6c03c81df29c0192443
stasastapov <elmoons@yandex.ru>
2024-10-29 23:21:49 +0300

    add unic users

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
+from src.api.auth import router as router_auth
 from src.config import settings
 
 from src.database import *
@@ -15,6 +16,7 @@ print(f"{settings.DB_URL}")
 
 app = FastAPI()
 
+app.include_router(router_auth)
 app.include_router(router_hotels)
 
 if __name__ == "__main__":
