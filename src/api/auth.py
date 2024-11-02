54187b4adb45ee96d2a920771d82e4aa8cbf5056
stasastapov <elmoons@yandex.ru>
2024-11-02 12:06:39 +0300

    start task logout

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, HTTPException, Response, Request
-from starlette.requests import cookie_parser
-
+from fastapi import APIRouter, HTTPException, Response
+from src.api.dependencies import UserIdDep
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserRequestAdd, UserAdd
@@ -32,13 +31,10 @@ async def login_user(data: UserRequestAdd, response: Response):
         return {"access_token": access_token}
 
 
-@router.get("/only_auth")
-async def only_auth(
-        request: Request,
+@router.get("/me")
+async def get_me(
+        user_id: UserIdDep,
 ):
-    access_token = request.cookies.get("access_token")
-    data = AuthService().decode_jwt(access_token)
-    user_id = data["user_id"]
     async with async_session_maker() as session:
         user = await UsersRepository(session).get_one_or_none(id=user_id)
     return user
