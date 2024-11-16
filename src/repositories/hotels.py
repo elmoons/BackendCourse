657a0ef3a0e201de00350d95b4ced2cdec8ca349
stasastapov <elmoons@yandex.ru>
2024-11-16 22:11:26 +0300

    task return pagination

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -42,3 +42,4 @@ class HotelsRepository(BaseRepository):
         result = await self.session.execute(query)
         return [Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
 
+
