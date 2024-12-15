155ed1c1588a156ae5b55d1a1d88439c23aa8033
stasastapov <elmoons@yandex.ru>
2024-12-15 18:28:00 +0300

    celery beat commit

--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from sqlalchemy import insert, select
 
 from src.models.bookings import BookingsOrm
@@ -10,3 +12,11 @@ from src.schemas.bookings import Booking
 class BookingsRepository(BaseRepository):
     model = BookingsOrm
     mapper = BookingDataMapper
+
+    async def get_bookings_with_today_checkin(self):
+        query = (
+            select(BookingsOrm)
+            .filter(BookingsOrm.date_from == date.today())
+        )
+        res = await self.session.execute(query)
+        return [self.mapper.map_to_domain_entity(booking) for booking in res.scalars().all()]
