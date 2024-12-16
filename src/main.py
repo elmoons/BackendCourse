f09bb6d7cd7a201aa93d2534d7a753d1d9a7d8fa
stasastapov <elmoons@yandex.ru>
2024-12-16 20:02:25 +0300

    fastapi background tasks complete

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
@@ -8,8 +9,10 @@ from fastapi_cache.backends.redis import RedisBackend
 import sys
 from pathlib import Path
 
+
 sys.path.append(str(Path(__file__).parent.parent))
 
+from src.api.dependencies import get_db
 from src.init import redis_manager
 from src.api.hotels import router as router_hotels
 from src.api.auth import router as router_auth
@@ -19,8 +22,21 @@ from src.api.facilities import router as router_facilities
 from src.api.images import router as router_images
 
 
+async def send_emails_bookings_today_checkin():
+    async for db in get_db():
+        bookings = await db.bookings.get_bookings_with_today_checkin()
+        print(f"{bookings=}")
+
+
+async def run_send_email_regulary():
+    while True:
+        await send_emails_bookings_today_checkin()
+        await asyncio.sleep(5)
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    asyncio.create_task(run_send_email_regulary())
     await redis_manager.connect()
     FastAPICache.init(RedisBackend(redis_manager.redis), prefix="fastapi-cache")
     yield
